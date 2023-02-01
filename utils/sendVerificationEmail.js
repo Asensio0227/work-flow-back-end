556168c5b1d65b45cca0c13161fe9248bf46de0e
Brendon Banda <83821620+Asensio0227@users.noreply.github.com>
2023-02-01 22:54:34 +0200

    Update sendVerificationEmail.js

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -2,7 +2,7 @@ const sendEmail = require('./sendEmail');
 
 const sendVerificationEmail = async ({ name, email, origin, verificationToken }) => {
   const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`
-  const message = `<p>Please confirm your email by clicking thew following link :
+  const message = `<p>Please confirm your email by clicking the following link :
   <a href="${verifyEmail}">verify email</a>
   </p>` ;
   return sendEmail({
